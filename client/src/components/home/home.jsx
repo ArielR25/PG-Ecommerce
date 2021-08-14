b51017b2d0872364ab1db92220d894ebe857ec6e
ArielR25 <arielrom27@gmail.com>
2021-08-14 12:56:14 -0300

    add(components/home) agregada funcionalidad barra de menu izquierda

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -4,6 +4,26 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllBooks } from "../../Actions/index";
 import Producto from "../producto/producto";
 import Filter from '../filter/filter';
+import toRight from '../../img/toRight.png';
+import toLeft from '../../img/toLeft.png';
+
+let leftBarState = false;
+
+function leftBarFunction(){
+  
+  let leftNavBar = document.getElementById('leftNavBar');
+  let leftNavIcon = document.getElementById('leftNavIcon');
+
+  if( leftBarState ){
+    leftNavIcon.src = toRight;
+    leftNavBar.style.left = '-400px';
+    leftBarState = false;
+  }else{
+    leftNavIcon.src = toLeft;
+    leftNavBar.style.left = '0px';
+    leftBarState = true;
+  }
+}
 
 export function Home () {
     const dispatch = useDispatch()
@@ -23,31 +43,48 @@ export function Home () {
   };
 
   return (
-    <div className="home">
-      <Filter/>
-      <div className="paginado">
-        {currentPage > 0 ? (
-          <button className="botonPrev" onClick={prevPage}>
-            Prev Page
-          </button>
-        ) : null}
-        {currentPage < 80 ? (
-          <button className="botonNext" onClick={nextPage}>
-            Next Page
-          </button>
-        ) : null}
+    <div className='principalHome'>
+      <div className='mainNavBar'>
+        <button id='leftNavBarButton' className='leftNavBarButton' onClick={ leftBarFunction }>
+          <img id='leftNavIcon' src={toLeft} alt="panel dysplay icon" />
+        </button>
+        <div id='leftNavBar' className='leftNavBar'>
+          
+          <p>soy el leftNavBar div</p>
+        </div>
+        <div className='searchMenu'>
+          <p>soy el primer div</p>
+        </div>
+        <div className='rightNavBar'>
+          <p>soy rightNavBar</p>
+        </div>
       </div>
+      <div className="home">
+        <Filter/>
+        <div className="paginado">
+          {currentPage > 0 ? (
+            <button className="botonPrev" onClick={prevPage}>
+              Prev Page
+            </button>
+          ) : null}
+          {currentPage < 80 ? (
+            <button className="botonNext" onClick={nextPage}>
+              Next Page
+            </button>
+          ) : null}
+        </div>
 
-      <div className="books">
-        {librosIniciales.map((e, index) => (
-          <Producto
-            key={index + 1}
-            titulo={e.titulo}
-            img={e.img}
-            autor={e.autor}
-            precio={e.precio}
-          />
-        ))}
+        <div className="books">
+          {librosIniciales.map((e, index) => (
+            <Producto
+              key={index + 1}
+              titulo={e.titulo}
+              img={e.img}
+              autor={e.autor}
+              precio={e.precio}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
