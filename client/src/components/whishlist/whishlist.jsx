4f423cd021f42d0b5a789b82d792eee21d5d5574
= <matiassundblad45@hotmail.com>
2021-09-23 18:53:22 -0300

    cambios para deploy

--- client/src/components/whishlist/whishlist.jsx
+++ client/src/components/whishlist/whishlist.jsx
@@ -1,34 +1,32 @@
-import React,{ useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {getWhishlist, deleteWhishlist} from '../../Actions/index'
-import Producto from '../producto/producto.jsx'
-import './whishlist.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getWhishlist } from "../../Actions/index";
+import Producto from "../producto/producto.jsx";
+import "./whishlist.css";
 
-export default function Whishlist () {
-    
-    const token= window.localStorage.getItem('token')
-    const dispatch = useDispatch()
-    const state = useSelector(state => state.whishlist)
-    const url = useSelector(state => state.url)
+export default function Whishlist() {
+  const token = window.localStorage.getItem("token");
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.whishlist);
+  useSelector((state) => state.url);
 
+  useEffect(() => {
+    dispatch(getWhishlist(token));
+  }, [state, token, dispatch]);
 
-   
-
-    useEffect(() => {
-        dispatch(getWhishlist(token))
-    }, [state])
-
-    return (
-        <div className="wishlist_contenido">
-           {state.map(e=><Producto 
-           key={e.producto._id}
-           titulo={e.producto.titulo} 
-           autor={e.producto.autor} 
-           img={e.producto.img} 
-           precio={e.producto.precio}
-           id={e.producto._id}
-           stock={'whishlist'}
-            />)}
-        </div>
-    )
+  return (
+    <div className="wishlist_contenido">
+      {state.map((e) => (
+        <Producto
+          key={e.producto._id}
+          titulo={e.producto.titulo}
+          autor={e.producto.autor}
+          img={e.producto.img}
+          precio={e.producto.precio}
+          id={e.producto._id}
+          stock={"whishlist"}
+        />
+      ))}
+    </div>
+  );
 }
--- client/src/funciones/login/newUser.js
+++ client/src/funciones/login/newUser.js
@@ -1,16 +1,14 @@
-export default async function newUser (data){
-    
-    var user = await fetch('http://localhost:4000/auth',{
-        method: 'POST',
-        headers:{
-            'Accept': 'application/json',
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    
-        const res = await user.json();
+export default async function newUser(data) {
+  var user = await fetch("https://pg-henry-ecommerce.herokuapp.com/auth", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
-        return res
-    
-}//guardar en la carpeta nueva
+  const res = await user.json();
+
+  return res;
+} //guardar en la carpeta nueva
